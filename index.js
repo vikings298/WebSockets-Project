9068f440f83a2a5887cf1ce7f3e0c3da49360a5d
Mitchell McNaney <mmcnaney298@gmail.com>
2026-03-15 21:34:08 -0500

    added websocket client

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const WebSocket = require("ws");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("WebSocket server is running");
+
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "public", "index.html");
+
+    fs.readFile(filePath, (err, content) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    });
+
+    return;
+  }
+
 });
+
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
-  ws.send("Hello from server");
+  ws.send("Connected to server");
 
   ws.on("message", (message) => {
     console.log("Received:", message.toString());
-    ws.send(`Server received: ${message}`);
+    ws.send("Server received: " + message);
   });
 
   ws.on("close", () => {
@@ -23,6 +36,7 @@ wss.on("connection", (ws) => {
 });
 
 const PORT = 8080;
+
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
